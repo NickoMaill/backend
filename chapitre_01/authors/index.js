58ba78911d68c80a3cab484c47eca3c38435a583
Nicko <nickos@iMac.local>
2022-03-07 15:25:52 +0100

    put data in seperate JSON file

--- chapitre_01/authors/index.js
+++ chapitre_01/authors/index.js
@@ -1,28 +1,6 @@
 const express = require("express");
 const app = express();
-
-const authors = [
-    {
-        name: "Lawrence Nowell",
-        nationality: "UK",
-        books: ["Beowulf"]
-    },
-    {
-        name: "William Shakespeare",
-        nationality: "UK",
-        books: ["Hamlet", "Othello", "Romeo and Juliet", "MacBeth"]
-    },
-    {
-        name: "Charles Dickens",
-        nationality: "US",
-        books: ["Oliver Twist", "A Christmas Carol"]
-    },
-    {
-        name: "Oscar Wilde",
-        nationality: "UK",
-        books: ["The Picture of Dorian Gray", "The Importance of Being Earnest"]
-    },
-]
+const authors = require("./data/authors.json")
 
 app.get("/", (req, res) => {
     res.send("Authors API");
