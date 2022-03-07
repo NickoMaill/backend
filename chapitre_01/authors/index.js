75fe62304a4f180efd05b68c3cf7c98277013f08
Nicko <nickos@iMac.local>
2022-03-07 14:03:40 +0100

    exo1 ok

---
+++ chapitre_01/authors/index.js
@@ -0,0 +1,12 @@
+const express = require("express"); 
+const app = express(); 
+
+app.get("/", (req, res) => {
+
+    res.send("Authors API");
+
+});
+
+app.listen(8000, () => { 
+    console.log("Listening on port 8000");
+});
